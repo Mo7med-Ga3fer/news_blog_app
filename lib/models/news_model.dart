024293106923bb12d13c7ac863c503ba11c448b0
Mo7med-Ga3fer <mo7medsala71403@gmail.com>
2023-08-03 15:49:51 +0300

    finish app in dealing with api

--- lib/models/news_model.dart
+++ lib/models/news_model.dart
@@ -20,7 +20,7 @@ class NewsModel
     return NewsModel(
       title: jsonData['title'] ?? '',
       urlToImage: jsonData['urlToImage'] ?? 'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg',
-      publishedAt: DateFormat.yMMMd().format(DateTime.parse(jsonData['publishedAt'])).toString() ?? '',
+      publishedAt: DateFormat.yMMMd().format(DateTime.parse(jsonData['publishedAt'])).toString(),
       content: jsonData['content'] ?? '',
       author: jsonData['author'] ?? '',
       );
